98a295f8fd6450ff4b89179a9efece26d2cc7759
Adam Rutkowski <hq@mtod.org>
2020-01-25 13:20:30 +0100

    Initial commit

---
+++ lib/mnemo.ex
@@ -0,0 +1,69 @@
+defmodule Mnemo do
+  def mnemonic(entropy) do
+    entropy
+    |> decode()
+    |> assert_size()
+    |> update_with_checksum()
+    |> sentence()
+    |> Enum.map(&word/1)
+    |> Enum.join(" ")
+  end
+
+  def decode(ent), do: Base.decode16!(ent, case: :mixed)
+
+  def assert_size(ent) when bit_size(ent) >= 128 and bit_size(ent) <= 256, do: ent
+  def assert_size(_ent), do: raise("ENT must be 128-256 bits")
+
+  def update_with_checksum(ent) do
+    {checksum, checksum_size} = checksum(ent)
+    <<ent::binary, checksum::size(checksum_size)>>
+  end
+
+  def sentence(ent_cs), do: chunks(ent_cs, 11)
+
+  def chunks(binary, n) do
+    do_chunks(binary, n, [])
+  end
+
+  def decode_integer(b) when is_bitstring(b) do
+    b
+    |> pad_leading_zeros()
+    |> :binary.decode_unsigned(:big)
+  end
+
+  def word(i, lang \\ :english) when i in 0..2047 do
+    "priv/#{lang}.txt"
+    |> File.stream!()
+    |> Stream.with_index()
+    |> Stream.filter(fn {_value, index} -> index == i end)
+    |> Enum.at(0)
+    |> elem(0)
+    |> String.trim()
+  end
+
+  def checksum(ent) do
+    s = div(bit_size(ent), 32)
+    {part(:crypto.hash(:sha256, ent), s), s}
+  end
+
+  def pad_leading_zeros(bs) when is_binary(bs), do: bs
+
+  def pad_leading_zeros(bs) when is_bitstring(bs) do
+    pad_length = 8 - rem(bit_size(bs), 8)
+    <<0::size(pad_length), bs::bitstring>>
+  end
+
+  defp do_chunks(binary, n, acc) when bit_size(binary) <= n do
+    Enum.reverse([decode_integer(<<binary::bitstring>>) | acc])
+  end
+
+  defp do_chunks(binary, n, acc) do
+    <<chunk::size(n), rest::bitstring>> = binary
+    do_chunks(rest, n, [decode_integer(<<chunk::size(n)>>) | acc])
+  end
+
+  defp part(bin, n) do
+    <<x::integer-size(n), _t::bitstring>> = bin
+    x
+  end
+end
---
+++ test/mnemo_test.exs
@@ -0,0 +1,94 @@
+defmodule MnemoTest do
+  use ExUnit.Case
+  doctest Mnemo
+
+  import Mnemo
+
+  @vectors File.read!("priv/vectors.json") |> Jason.decode!() |> Map.fetch!("english")
+
+  test "Vectors" do
+    for [entropy, mnemonic | _] <- @vectors do
+      IO.write(IO.ANSI.reset() <> "Vector: #{entropy}")
+      assert mnemonic(entropy) == mnemonic
+      IO.write(IO.ANSI.green() <> " ...OK\n")
+    end
+  end
+
+  describe "Words" do
+    test "English words can be retrieved by index" do
+      assert word(0) == "abandon"
+      assert word(2047) == "zoo"
+    end
+  end
+
+  describe "Relations between entropy, checksum and length of mnemonic sentence" do
+    test "ENT+CS=132 (12)", do: assert_ms(132, 12)
+
+    test "ENT+CS=165 (15)", do: assert_ms(165, 15)
+
+    test "ENT+CS=198 (18)", do: assert_ms(198, 18)
+
+    test "ENT+CS=231 (21)", do: assert_ms(231, 21)
+
+    test "ENT+CS=264 (24)", do: assert_ms(264, 24)
+
+    test "ENT=128, CS=4", do: assert_cs(128, 4)
+
+    test "ENT=160, CS=5", do: assert_cs(160, 5)
+
+    test "ENT=192, CS=6", do: assert_cs(192, 6)
+
+    test "ENT=224, CS=7", do: assert_cs(224, 7)
+
+    test "ENT=256, CS=8", do: assert_cs(256, 8)
+
+    defp assert_ms(ent_cs, ms) do
+      ^ms = ceil(bit_size(<<1::size(ent_cs)>>) / 11)
+      assert length(sentence(<<1::size(ent_cs)>>)) == ms
+    end
+
+    defp assert_cs(ent, cs), do: assert {_, ^cs} = checksum(<<1::size(ent)>>)
+  end
+
+
+  describe "Bits" do
+    test "chunks" do
+      assert [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        3
+      ] = chunks(<<0::128, 3::4>>, 11)
+    end
+
+    test "pad_leading_zeros/1" do
+      assert pad_leading_zeros(<<0>>) == <<0>>
+      assert pad_leading_zeros(<<100>>) == <<100>>
+      assert pad_leading_zeros(<<0::1>>) == <<0>>
+      assert pad_leading_zeros(<<20::7>>) == <<20>>
+      assert pad_leading_zeros(<<0, 0::4>>) == <<0, 0>>
+    end
+
+    test "decode_integer/1" do
+      assert decode_integer(<<0>>) == 0
+      assert decode_integer(<<0::1>>) == 0
+      assert decode_integer(<<0::2>>) == 0
+      assert decode_integer(<<0::3>>) == 0
+      assert decode_integer(<<0::4>>) == 0
+      assert decode_integer(<<0::5>>) == 0
+      assert decode_integer(<<0::6>>) == 0
+      assert decode_integer(<<0::7>>) == 0
+      assert decode_integer(<<1>>) == 1
+      assert decode_integer(<<1::16>>) == 1
+      assert decode_integer(<<1, 0::4>>) == 16
+    end
+  end
+end
